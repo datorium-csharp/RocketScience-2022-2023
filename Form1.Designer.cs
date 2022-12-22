88c2c3690b4ff11d9ac2939fefabfe58c2474051
& C:/Users/Elchin/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Elchin/OneDrive/Documents/Codes/Python/json-thu1200/script.py <hello@datorium.eu>
2022-12-22 19:31:44 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace RocketScience
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rocket = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.rocket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rocket
+            // 
+            this.rocket.Image = global::RocketScience.Properties.Resources.rocket_off_000;
+            this.rocket.Location = new System.Drawing.Point(454, 553);
+            this.rocket.Name = "rocket";
+            this.rocket.Size = new System.Drawing.Size(100, 151);
+            this.rocket.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.rocket.TabIndex = 0;
+            this.rocket.TabStop = false;
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1054, 769);
+            this.Controls.Add(this.rocket);
+            this.Name = "Game";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Game_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.rocket)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox rocket;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using RocketScience.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Resources;
+
+namespace RocketScience
+{
+    public partial class Game : Form
+    {
+        Timer gameTimer = null;
+        Timer rocketTimer = null;
+        int horVelocity = 0;
+        int rocketImageCounter = 0;
+
+        public Game()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            gameTimer = new Timer();
+            gameTimer.Interval = 10;
+            gameTimer.Tick += GameTimer_Tick;
+            gameTimer.Start();
+
+            rocketTimer = new Timer();
+            rocketTimer.Interval = 50;
+            rocketTimer.Tick += RocketTimer_Tick;
+            rocketTimer.Start();
+
+            this.KeyDown += Game_KeyDown;
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            rocket.Left += horVelocity;
+        }
+
+        private void RocketTimer_Tick(object sender, EventArgs e)
+        {
+            string rocketImage = $"rocket_off_00{rocketImageCounter}";
+            rocket.Image = (Image)Resources.ResourceManager.GetObject(rocketImage);
+            rocketImageCounter += 1;
+            if (rocketImageCounter > 3) rocketImageCounter = 0;
+        }
+
+        private void Game_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                horVelocity = -5;
+            }
+            else if(e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                horVelocity = 5;
+            }
+        }
+
+        private void Game_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
